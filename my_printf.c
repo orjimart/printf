98f90079eb3fe6796b883daf766cbf857071dd4f
orjimart <orjimart@gmail.com>
2023-10-17 15:44:35 -0700

    update

--- my_printf.c
+++ my_printf.c
@@ -1,52 +1,46 @@
 #include "main.h"
 /**
- * _printf - A function that prints formatted output
- * @format: The format string
- * Return: The number of characters printed
+ * _printf - function that produces output according to a format
+ * @format: strintg to be formatted
+ * Return: return the exact strings produced
  */
 int _printf(const char *format, ...)
 {
-    int char_count = 0;
-    va_list arg_list;
-    char *format_ptr, *start;
-    param_t param = PARAMS_INIT;
+	unsigned int mart = 0, a, enumerate = 0;
+	va_list args;
 
-    va_start(arg_list, format);
+	if (!format || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
+	va_start(args, format);
 
-    if (!format || (format[0] == '%' && !format[1]))
-        return (-1);
-
-    if (format[0] == '%' && format[1] == ' ' && !format[2])
-        return (-1);
-
-    for (format_ptr = (char *)format; *format_ptr; format_ptr++)
-    {
-        init_param(&param, arg_list);
-        if (*format_ptr != '%') /* Check for characters not starting with '%' */
-        {
-            char_count += our_putchar(*format_ptr);
-            continue;
-        }
-        start = format_ptr;
-        format_ptr++;
-        while (get_flags(format_ptr, &param)) /* Handle flag characters */
-        {
-            format_ptr++;
-        }
-        format_ptr = _width(format_ptr, &param, arg_list);
-        format_ptr = _precision(format_ptr, &param, arg_list);
-        if (_modifier(format_ptr, &param))
-            format_ptr++;
-        if (!list_specifiers(format_ptr))
-            char_count += print_from_to(start, format_ptr,
-                param.l_modifier || param.h_modifier ? format_ptr - 1 : 0);
-        else
-            char_count += _print_functions(format_ptr, arg_list, &param);
-    }
-
-    _putchar(BUF_FLUSH);
-    va_end(arg_list);
-
-    return (char_count);
+	for (a = 0; format[a] != '\0'; a++)
+	{
+		if (format[a] != '%')
+		{
+		our_putchar(format[a]);
+		}
+		else if (format[a] == '%' && format[a + 1] == 'c')
+		{
+			our_putchar(va_arg(args, int));
+			a++;
+		}
+		else if (format[a] == '%' && format[a + 1] == 's')
+			{
+			enumerate = di_puts(va_arg(args, char *));
+			mart += (enumerate - 1);
+			a++;
+			}
+		else if (format[a] == '%' && (format[a + 1] == '%'))
+			{
+			our_putchar('%');
+			a++;
+			}
+		else
+		{
+		our_putchar('%');
+		}
+		mart += 1;
+	}
+		va_end(args);
+		return (mart);
 }
-
