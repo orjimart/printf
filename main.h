b1d5aed0d403f47998de0856126e5b0e90d64ecc
orjimart <orjimart@gmail.com>
2023-10-17 15:50:31 -0700

    update main.h

--- main.h
+++ main.h
@@ -21,8 +21,4 @@ int di_prints(int in);
 int _printf(const char *format, ...);
 int our_putchar(char cha);
 int di_puts(char *strng);
-int (*list_specifiers(char *strng))(va_list arg, param_t *param);
-int _print_function(char *strng, va_list arg, param_t *param);
-int _modifier(char *strng, param_t *param);
-char *_width(char *strng, param_t *param, va_list arg);
 #endif
