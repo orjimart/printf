5077e06c0d2507a52b0a2082f0d10ce8171817d2
orjimart <orjimart@gmail.com>
2023-10-16 17:24:34 -0700

    main.h

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdarg.h>
+
+int di_prints(int in);
+int _printf(const char *format, ...);
+int our_putchar(char cha);
+int di_puts(char *strng);
+#endif
